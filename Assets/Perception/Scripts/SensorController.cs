2dcd01327280289e3b352277b4a8a876b47b5fb6
Giancarlo Sanchez <giancarlosanchez@Giancarlos-MacBook-Pro.local>
2015-10-15 19:08:44 +0100

    Balance in multiple axis

--- Assets/Perception/Scripts/SensorController.cs
+++ Assets/Perception/Scripts/SensorController.cs
@@ -5,9 +5,11 @@ public class SensorController : MonoBehaviour {
 
 	public Transform motorSensor;
 
-
+	private float targetAccel  = 0;
 	private float acceleration = 0;
 
+	private float hAccel = 0;
+	private float angleY = 0;
 	// Use this for initialization
 	void Start () {
 	
@@ -17,23 +19,40 @@ public class SensorController : MonoBehaviour {
 	void Update () {
 
 		//Forward/Backwards
-		acceleration = -motorSensor.forward.y*10f;
+		targetAccel = -motorSensor.forward.y*Mathf.Exp(2);
+		hAccel = -motorSensor.right.y * Mathf.Exp (2);
 
+		if (targetAccel <= -4f) {
+			targetAccel *= 0.3f;
+		} else if (targetAccel <= 1.5f) {
+			targetAccel *= 0f;
+		} else if (targetAccel <= 2.25f) {
+			targetAccel *= 0.3f;
+		} else if (targetAccel <= 3f) {
+			targetAccel *= 0.6f;
+		} else {
+			targetAccel *= 1.0f;
+		}
 
-		transform.Translate (Vector3.forward*acceleration * Time.deltaTime);
+		acceleration = Mathf.Lerp (acceleration, targetAccel, Time.time*0.1f);
 
-		float angleY = Mathf.LerpAngle(transform.eulerAngles.y, motorSensor.eulerAngles.y, Time.deltaTime*3f);
+		//Rotate body to follow neck twist
+		angleY = Mathf.LerpAngle(transform.eulerAngles.y, motorSensor.eulerAngles.y, Time.deltaTime*3f);
 		transform.eulerAngles = new Vector3(transform.eulerAngles.x, angleY, transform.eulerAngles.z);
+
+		//Move towards the head balance
+		transform.Translate ((Vector3.right*hAccel+Vector3.forward*acceleration) * Time.deltaTime);
+
 	}
 
 
 	void OnGUI(){
-		GUI.Label(new Rect(90, 90, 300, 20), "Rot Accel = "+transform.localEulerAngles);
-		GUI.Label(new Rect(90, 110, 300, 20), "Rot Rate = "+motorSensor.localEulerAngles);
-		GUI.Label(new Rect(90, 130, 300, 20), "angle = "+ motorSensor.forward.y);
+		GUI.Label(new Rect(90, 90, 300, 20), "Rot Accel = "+motorSensor.eulerAngles);
+		GUI.Label(new Rect(90, 110, 300, 20), "Rot Rate z = "+-motorSensor.eulerAngles.z);
+		GUI.Label(new Rect(90, 130, 300, 20), "angle = "+ -motorSensor.forward.y*Mathf.Exp(2)/2);
 		
 		GUI.Label(new Rect(90, 150, 300, 20), "Accel magnitude = "+Input.acceleration.magnitude);
-		GUI.Label(new Rect(90, 170, 300, 20), "Accel = "+Input.acceleration);
-		GUI.Label(new Rect(90, 270, 300, 20), "Rotation = "+motorSensor.rotation.x);
+		GUI.Label(new Rect(90, 170, 300, 20), "H accel = "+hAccel);
+		GUI.Label(new Rect(90, 270, 300, 20), "Angle = "+Vector3.Angle(Vector3.forward, motorSensor.right));
 	}
 }
